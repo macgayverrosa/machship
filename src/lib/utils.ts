6faf4e2dd31f9d3bf313115043fad7138171db05
Macgayver Rosa <macgayver.rosa@outlook.com>
2024-02-06 17:23:33 +1100

    installing clsx

---
+++ src/lib/utils.ts
@@ -0,0 +1,50 @@
+import { type ClassValue, clsx } from 'clsx'
+import { Metadata } from 'next'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function constructMetadata({
+  title = 'MyMate - the marketplace for digital assets',
+  description = 'MyMate is an open-source marketplace for high-quality digital goods.',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '',
+    },
+    icons,
+    metadataBase: new URL('https://mymates.up.railway.app'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
+}
