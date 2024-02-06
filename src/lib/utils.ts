f0fd4c5ad41753d1bc386bb44337e942c1ae8d93
Macgayver Rosa <macgayver.rosa@outlook.com>
2024-02-06 17:41:18 +1100

    installing shadcn ui

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,50 +1,6 @@
-import { type ClassValue, clsx } from 'clsx'
-import { Metadata } from 'next'
-import { twMerge } from 'tailwind-merge'
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function constructMetadata({
-  title = 'MachShip Management Portal',
-  description = 'MachShip Management Portal',
-  image = '/thumbnail.png',
-  icons = '/favicon.ico',
-  noIndex = false,
-}: {
-  title?: string
-  description?: string
-  image?: string
-  icons?: string
-  noIndex?: boolean
-} = {}): Metadata {
-  return {
-    title,
-    description,
-    openGraph: {
-      title,
-      description,
-      images: [
-        {
-          url: image,
-        },
-      ],
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title,
-      description,
-      images: [image],
-      creator: '',
-    },
-    icons,
-    metadataBase: new URL('https://machship.up.railway.app'),
-    ...(noIndex && {
-      robots: {
-        index: false,
-        follow: false,
-      },
-    }),
-  }
 }
