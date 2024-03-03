d24f32b397d64f427461eebe6481c6d03598d82b
Macgayver Rosa <macgayver.rosa@outlook.com>
2024-03-03 12:49:40 +1100

    create custom dashboard

--- src/components/custom/CustomTable.tsx
+++ src/components/custom/CustomTable.tsx
@@ -1,37 +1,38 @@
-import { promises as fs } from "fs"
-import path from "path"
+import { promises as fs } from "fs";
+import path from "path";
 // import Image from "next/image"
-import { z } from "zod"
-import { DataTable } from "../data-table/data-table"
+import { z } from "zod";
+import { DataTable } from "../data-table/data-table";
 // import { UserNav } from "@/components/layout/NavUser"
-import { taskSchema } from "../../app/devs/data/schema"
-import { Button } from "../ui/button"
-import { ArrowLeftCircle } from "lucide-react"
-import { useRouter } from "next/navigation"
+import { taskSchema } from "../../app/devs/data/schema";
+import { Button } from "../ui/button";
+import { ArrowLeftCircle } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 // Simulate a database read for tasks.
 async function getTasks() {
-
   const data = await fs.readFile(
     path.join(process.cwd(), "src/app/devs/data/tasks.json")
-  )
+  );
 
-  const tasks = JSON.parse(data.toString())
+  const tasks = JSON.parse(data.toString());
 
-  return z.array(taskSchema).parse(tasks)
+  return z.array(taskSchema).parse(tasks);
 }
 
 interface CustomTableProps {
-  tool: string
-  columns: any
-  filterBy: any
+  tool: string;
+  columns: any;
+  filterBy: any;
 }
 
-export default async function CustomTable(
-  {tool, columns, filterBy}: CustomTableProps
-) {
-  const data = await getTasks()
+export default async function CustomTable({
+  tool,
+  columns,
+  filterBy,
+}: CustomTableProps) {
+  const data = await getTasks();
 
   const addPath = `/${tool}/add`;
   const nameButton = tool.slice(0, -1);
@@ -50,12 +51,14 @@ export default async function CustomTable(
   //   // eslint-disable-next-line react-hooks/exhaustive-deps
   // }, []);
 
-
   return (
-    <div className="hidden h-full flex-1 flex-col space-y-8 pt-5 md:flex">
-      <div className="flex items-center justify-between space-y-2">
+    <div className="grid gap-4">
+      <div className="hidden h-full flex-1 flex-col space-y-8 pt-5 md:flex">
+        <div className="flex items-center justify-between space-y-2">
           <div>
-            <h2 className="text-2xl font-bold tracking-tight capitalize">{tool} Datatable</h2>
+            <h2 className="text-2xl font-bold tracking-tight capitalize">
+              {tool} Datatable
+            </h2>
             <p className="text-muted-foreground">
               Here&apos;s a list of all your {tool}!
             </p>
@@ -66,16 +69,16 @@ export default async function CustomTable(
             </Button> */}
           </div>
         </div>
-      {/* <DataTable data={tasks} columns={columns} /> */}
+        {/* <DataTable data={tasks} columns={columns} /> */}
 
-      <DataTable
+        <DataTable
           data={data}
           columns={columns}
           buttonAddPath={addPath}
           buttonAddName={nameButton}
           filterBy={filterBy}
         />
-
+      </div>
     </div>
-  )
+  );
 }
