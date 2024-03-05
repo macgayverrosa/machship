63a879411db451551b8f79e5567a6e818181e5a6
Macgayver Rosa <macgayver.rosa@outlook.com>
2024-03-06 10:15:33 +1100

    fixing actions in the table row

---
+++ src/app/devs/components/tableActions.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Eye, Pencil, Trash } from "lucide-react";
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+import { Row } from "@tanstack/react-table";
+import routes from "@/config/routes";
+
+interface ActionsProps<TData> {
+  row: Row<TData>;
+}
+
+export default function Actions<TData>({ row }: ActionsProps<TData>) {
+  const hrefEdit = '';//routes.EMPLOYEES_EDIT + row.getValue('_id')
+  const hrefDelete = '';//routes.EMPLOYEES_DELETE + row.getValue('_id')
+
+  return (
+    <DropdownMenuContent align="end" className="w-[160px]">
+      {/* <DropdownMenuItem>
+        <Eye className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+        View
+      </DropdownMenuItem> */}
+      {/* <DropdownMenuSeparator /> */}
+
+      <Link href={hrefEdit}>
+        <DropdownMenuItem>
+          <Pencil className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+          Edit
+        </DropdownMenuItem>
+      </Link>
+      <Link href={hrefDelete}>
+      <DropdownMenuItem>
+        <Trash className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+        Delete
+      </DropdownMenuItem>
+      </Link>
+    </DropdownMenuContent>
+  );
+}
--- src/app/devs/components/tableColumns.tsx
+++ src/app/devs/components/tableColumns.tsx
@@ -1,125 +1,108 @@
 "use client"
 
-import { ColumnDef } from "@tanstack/react-table"
-import { Badge } from "@/components/ui/badge"
-import { Checkbox } from "@/components/ui/checkbox"
+//import { Badge } from "@/components/ui/badge"
 
-import { DataTableColumnHeader } from "@/components/data-table/data-table-column-header"
-import { DataTableRowActions } from "@/components/data-table/data-table-row-actions"
 
 // UPDATE THOSE FIELDS
-import { labels, priorities, statuses } from "@/app/devs/data/data"
-import { Task } from "@/app/devs/data/schema"
+//import { labels, priorities, statuses } from "@/app/devs/data/data"
+//import { Task } from "@/app/devs/data/schema"
 
 
-export const columns: ColumnDef<Task>[] = [
+import { ColumnDef } from "@tanstack/react-table"
+import { Checkbox } from "@/components/ui/checkbox"
+import { ItemSchema } from "../data/schema"
+import { DataTableColumnHeader } from "@/components/data-table/data-table-column-header"
+import { DataTableRowActions } from "@/components/data-table/data-table-row-actions"
+import Actions from "./tableActions"
+import format from "date-fns/format"
+
+
+export const columns: ColumnDef<ItemSchema>[] = [
+  // {
+  //   id: "select",
+  //   header: ({ table }) => (
+  //     <Checkbox
+  //       checked={table.getIsAllPageRowsSelected()}
+  //       onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+  //       aria-label="Select all"
+  //       className="translate-y-[2px]"
+  //     />
+  //   ),
+  //   cell: ({ row }) => (
+  //     <Checkbox
+  //       checked={row.getIsSelected()}
+  //       onCheckedChange={(value) => row.toggleSelected(!!value)}
+  //       aria-label="Select row"
+  //       className="translate-y-[2px]"
+  //     />
+  //   ),
+  //   enableSorting: false,
+  //   enableHiding: false,
+  // },
+
   {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-        className="translate-y-[2px]"
-      />
+    id: "actions",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Options" />
+    ),    
+    cell: ({ row }) => <DataTableRowActions row={row} actions={Actions({row})} />,
+  },
+
+  // {
+  //   id: "actions",
+  //   cell: ({ row }) => <DataTableRowActions row={row} />,
+  // },
+
+
+  {
+    accessorKey: "_id",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="ID" className="hidden" />
     ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-        className="translate-y-[2px]"
-      />
-    ),
+    cell: ({ row }) => <div className="hidden">{row.getValue("_id")}</div>,
     enableSorting: false,
     enableHiding: false,
   },
   {
-    accessorKey: "id",
+    accessorKey: "name",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Task" />
+      <DataTableColumnHeader column={column} title="Name" />
     ),
     cell: ({ row }) => <div className="w-[80px]">{row.getValue("id")}</div>,
     enableSorting: false,
     enableHiding: false,
   },
-  {
-    accessorKey: "title",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Title" />
-    ),
-    cell: ({ row }) => {
-      const label = labels.find((label) => label.value === row.original.label)
 
-      return (
-        <div className="flex space-x-2">
-          {label && <Badge variant="outline">{label.label}</Badge>}
-          <span className="max-w-[500px] truncate font-medium">
-            {row.getValue("title")}
-          </span>
-        </div>
-      )
-    },
+
+  {
+    accessorKey: "address",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Address" />
+    ),
+  },
+  {
+    accessorKey: "phone",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Phone" />
+    ),
   },
   {
     accessorKey: "status",
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Status" />
     ),
-    cell: ({ row }) => {
-      const status = statuses.find(
-        (status) => status.value === row.getValue("status")
-      )
-
-      if (!status) {
-        return null
-      }
-
-      return (
-        <div className="flex w-[100px] items-center">
-          {status.icon && (
-            <status.icon className="mr-2 h-4 w-4 text-muted-foreground" />
-          )}
-          <span>{status.label}</span>
-        </div>
-      )
-    },
-    filterFn: (row, id, value) => {
-      return value.includes(row.getValue(id))
-    },
   },
+  // {
+  //   accessorKey: "start_date",
+  //   header: ({ column }) => (
+  //     <DataTableColumnHeader column={column} title="Date" />
+  //   ),
+  //   cell: ({ row }) => <div className="">{format(new Date(row.getValue("start_date")), "PPP")}</div>,
+  // },  
   {
-    accessorKey: "priority",
+    accessorKey: "business_id",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Priority" />
+      <DataTableColumnHeader column={column} title="Business ID" />
     ),
-    cell: ({ row }) => {
-      const priority = priorities.find(
-        (priority) => priority.value === row.getValue("priority")
-      )
-
-      if (!priority) {
-        return null
-      }
-
-      return (
-        <div className="flex items-center">
-          {priority.icon && (
-            <priority.icon className="mr-2 h-4 w-4 text-muted-foreground" />
-          )}
-          <span>{priority.label}</span>
-        </div>
-      )
-    },
-    filterFn: (row, id, value) => {
-      return value.includes(row.getValue(id))
-    },
-  },
-  {
-    id: "actions",
-    cell: ({ row }) => <DataTableRowActions row={row} />,
   },
 ]
